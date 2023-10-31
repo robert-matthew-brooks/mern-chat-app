1471ec452ed9f85ad6aa48ed15e9fac7433f5c67
bob <robert.matthew.brooks@gmail.com>
2023-10-31 12:38:52 +0000

    install cypress and add blank e2e test framework

---
+++ client/cypress/e2e/login.cy.js
@@ -0,0 +1,8 @@
+const validUsername = 'bob_test';
+const validPassword = 'testpass1';
+
+describe('login', () => {
+  it('should have the required login elements', () => {
+    cy.visit('/');
+  });
+});
