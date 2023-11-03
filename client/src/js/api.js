7b11789e90820f0ba2c4df93df4e4d1b14b364f7
bob <robert.matthew.brooks@gmail.com>
2023-11-03 12:31:07 +0000

    change backend port

--- client/src/js/api.js
+++ client/src/js/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'production'
     ? 'https://be-mern-chat-app.onrender.com/'
-    : 'http://localhost:6789';
+    : 'http://localhost:9090';
 
 const api = axios.create({
   baseURL,
