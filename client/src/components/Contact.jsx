41ab1de35f45add6a8b0a3a3f6be29123788445a
bob <robert.matthew.brooks@gmail.com>
2023-11-03 21:45:39 +0000

    add delete contact function

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,12 +1,28 @@
 import { useContext } from 'react';
 import { UserContext } from '../UserContext';
+import { removeContact } from '../js/api';
 import './Contact.css';
 
 export default function Contact({ contact, isOnline, isActive }) {
-  const { setActiveContact } = useContext(UserContext);
+  const { token, activeContact, setActiveContact, contacts, setContacts } =
+    useContext(UserContext);
 
   const handleContactClick = (contact) => {
     setActiveContact(contact);
+  };
+
+  const handleContactDelete = (evt, contactToDelete) => {
+    evt.stopPropagation();
+    if (contactToDelete.id === activeContact?.id) {
+      setActiveContact(null);
+    }
+
+    setContacts(
+      contacts.filter((contact) => {
+        return contact.id !== contactToDelete.id;
+      })
+    );
+    removeContact(contactToDelete.id, token);
   };
 
   return (
@@ -16,7 +32,7 @@ export default function Contact({ contact, isOnline, isActive }) {
         ${isOnline && 'Contact--online'}
         ${isActive && 'Contact--active'}
       `}
-      onClick={() => {
+      onClick={(evt) => {
         handleContactClick(contact);
       }}
     >
@@ -25,6 +41,28 @@ export default function Contact({ contact, isOnline, isActive }) {
         {contact.username[0]}
       </div>
       <div className="Contact__username">{contact.username}</div>
+      <div className="Contact__delete">
+        <button
+          onClick={(evt) => {
+            handleContactDelete(evt, contact);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
